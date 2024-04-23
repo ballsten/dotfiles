4e80494375c1578fe7b14c213076694740167b6f
Andrew Theaker <andrew@theaker.name>
2024-04-24 04:35:52 +0930

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,8 +9,9 @@ local config = wezterm.config_builder()
 local launch_menu = {}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   table.insert(launch_menu, {
-    label = 'PowerShell',
-    args = { 'pwsh.exe', '-NoLogo' },
+    label = 'New Tab (PowerShell)',
+    domain = { DomainName = "local" },
+    args = { 'pwsh.exe' },
   })
 end
 
@@ -19,8 +20,10 @@ config.launch_menu = launch_menu
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Sweet Love (terminal.sexy)'
+--config.color_scheme = 'Sweet Love (terminal.sexy)'
 --config.color_scheme = 'Summerfruit Light (base16)'
+config.color_scheme = 'Tokyo Night'
+
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 
 config.use_fancy_tab_bar = false
