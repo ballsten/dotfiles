5818ea3b5b7ad900456a0ce4201c943ea812ec8c
Andrew Theaker <andrew@theaker.name>
2024-04-29 14:34:41 +1000

    Minor updates configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,24 @@
+-- setup leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Basic editor config
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
+
+-- Additional config
+--
+-- Make line numbers default
+vim.g.have_nerd_font = true
+
+-- Make line numbers default
+vim.opt.number = true
+
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
 
 -- lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
