e73ddb3b696fcd795ac9c15cb1dae07b63a49a05
Andrew Theaker <andrew@theaker.name>
2024-04-29 21:47:20 +1000

    changes to lua config after typecraft episode 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,3 @@
--- setup leader keys
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Basic editor config
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Additional config
---
--- Make line numbers default
-vim.g.have_nerd_font = true
-
--- Make line numbers default
-vim.opt.number = true
-
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
-
 -- lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -34,27 +12,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
-
--- configure catppuccin colour scheme
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
--- configure telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- configure treesitter
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-  ensure_installed = { "lua", "javascript", "html", "python", "htmldjango"},
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },  
-})
-
--- configure Neotree
-vim.keymap.set('n', '<leader>fe', ':Neotree filesystem reveal left<CR>')
