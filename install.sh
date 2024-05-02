c50c80856410fe3f7c928449eaeb5b2a2e67ee69
Andrew Theaker <andrew@theaker.name>
2024-05-03 07:10:59 +1000

    add startship to dotfiles

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 # install nvim
 mkdir -p ~/.config
 ln -s $SCRIPTPATH/nvim ~/.config/nvim
+
+# install starship
+ln -s $SCRIPTPATH/starship.toml ~/.config/starship.toml
