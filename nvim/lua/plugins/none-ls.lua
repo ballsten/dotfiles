2aa51f2c6b1e365938eb0f509fb9289be44672a6
Andrew Theaker <andrew@theaker.name>
2024-05-02 15:55:07 +1000

    added additional python formatters

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,9 @@ return {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.black,
           null_ls.builtins.formatting.isort,
+          null_ls.builtins.formatting.djhtml,
+          null_ls.builtins.diagnostics.pylint,
+          null_ls.builtins.diagnostics.mypy,
         },
       })
 
